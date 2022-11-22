dba8c3ba8aed84695fcb91b44e4db7a8a3f5ae20
Dragonrock <38626407+Dragonrock@users.noreply.github.com>
2022-11-22 23:01:40 +0200

    new files

--- cpu.v
+++ cpu.v
@@ -1,55 +1,218 @@
-`include "lab4_library_input.v"
-`timescale 1ns/1ps
+/***********************************************************************************************/
+/*********************************  MIPS 5-stage pipeline implementation ***********************/
+/***********************************************************************************************/
 
+module cpu(input clock, input reset);
+ reg [31:0] PC; 
+ reg [31:0] IFID_PCplus4;
+ reg [31:0] IFID_instr;
+ reg [31:0] IDEX_rdA, IDEX_rdB, IDEX_signExtend;
+ reg [4:0]  IDEX_instr_rt, IDEX_instr_rs, IDEX_instr_rd;                            
+ reg        IDEX_RegDst, IDEX_ALUSrc;
+ reg [1:0]  IDEX_ALUcntrl;
+ reg        IDEX_Branch, IDEX_MemRead, IDEX_MemWrite; 
+ reg        IDEX_MemToReg, IDEX_RegWrite;                
+ reg [4:0]  EXMEM_RegWriteAddr, EXMEM_instr_rd; 
+ reg [31:0] EXMEM_ALUOut;
+ reg        EXMEM_Zero;
+ reg [31:0] EXMEM_MemWriteData;
+ reg        EXMEM_Branch, EXMEM_MemRead, EXMEM_MemWrite, EXMEM_RegWrite, EXMEM_MemToReg;
+ reg [31:0] MEMWB_DMemOut;
+ reg [4:0]  MEMWB_RegWriteAddr, MEMWB_instr_rd; 
+ reg [31:0] MEMWB_ALUOut;
+ reg        MEMWB_MemToReg, MEMWB_RegWrite;               
+ wire [31:0] instr, ALUInA, ALUInB, ALUOut, rdA, rdB, signExtend, DMemOut, wRegData, PCIncr;
+ wire Zero, RegDst, MemRead, MemWrite, MemToReg, ALUSrc, RegWrite, Branch;
+ wire [5:0] opcode, func;
+ wire [4:0] instr_rs, instr_rt, instr_rd, RegWriteAddr;
+ wire [3:0] ALUOp;
+ wire [1:0] ALUcntrl;
+ wire [15:0] imm;
 
-module CPU( input clock,reset);  
-
- reg[31:0] pc; 
- wire signed [31:0] pc_new, pc_step, pc_next;  
- wire [31:0] instr, extended, SrcB;  
- wire RegWrite, RegDst, AluSrc, Branch, MemWrite, MemToReg, MemRead, BranchCond, zero, pcDefiner;  
- wire [3:0] aluControl;
- wire [4:0] ra_registers_A, ra_registers_B, wa;
- wire  [31:0] rd_registers_A, rd_registers_B;
- wire [31:0] ALU_out, writeRegData, writeMemData, readData;
- wire [31:0] instructions;
- wire [4:0] writenReg;
-
- Memory cpu_IMem(clock, reset, 1'b1, 1'b0, pc >> 2, 32'b0, instructions);
-
- Memory cpu_DataMem(clock, reset, MemRead , MemWrite, ALU_out, rd_registers_B, readData);
-
- fsm fsMachine(RegWrite, RegDst, AluSrc, Branch, MemWrite, MemRead, MemToReg, BranchCond, aluControl, instructions[31:26], instructions[5:0]);
  
- RegFile cpu_regs(clock, reset,  instructions[25:21],  instructions[20:16], writenReg, RegWrite, writeRegData, rd_registers_A, rd_registers_B);
-
- ALU alu(ALU_out, zero, rd_registers_A, SrcB, aluControl);
-
-
- //Extension
- assign extended = { {16{instructions[15]}}, instructions[15:0]} ;
  
-//Signal Multiplexers
 
- assign SrcB= (AluSrc == 1'b1) ? extended : rd_registers_B;
- 
- assign writenReg = (RegDst == 1'b1) ? instructions[15:11] : instructions[20:16];
- 
- assign writeRegData = (MemToReg == 1'b1) ? readData : ALU_out;
+/***************** Instruction Fetch Unit (IF)  ****************/
+ always @(posedge clock or negedge reset)
+  begin 
+    if (reset == 1'b0)     
+       PC <= -1;     
+    else if (PC == -1)
+       PC <= 0;
+    else 
+       PC <= PC + 4;
+  end
+  
+  // IFID pipeline register
+ always @(posedge clock or negedge reset)
+  begin 
+    if (reset == 1'b0)     
+      begin
+       IFID_PCplus4 <= 32'b0;    
+       IFID_instr <= 32'b0;
+    end 
+    else 
+      begin
+       IFID_PCplus4 <= PC + 32'd4;
+       IFID_instr <= instr;
+    end
+  end
+  
+// TO FILL IN: Instantiate the Instruction Memory here 
+  
+  
+  
 
- // PC   
- always @(posedge clock or negedge reset)  
- begin   
-      if(!reset)  
-           pc <= 32'h0;  
-      else  
-           pc <= pc_next;  
- end
+/***************** Instruction Decode Unit (ID)  ****************/
+assign opcode = IFID_instr[31:26];
+assign func = IFID_instr[5:0];
+assign instr_rs = IFID_instr[25:21];
+assign instr_rt = IFID_instr[20:16];
+assign instr_rd = IFID_instr[15:11];
+assign imm = IFID_instr[15:0];
+assign signExtend = {{16{imm[15]}}, imm};
+
+// Register file
+RegFile cpu_regs(clock, reset, instr_rs, instr_rt, MEMWB_RegWriteAddr, MEMWB_RegWrite, wRegData, rdA, rdB);
+
+  // IDEX pipeline register
+ always @(posedge clock or negedge reset)
+  begin 
+    if (reset == 1'b0)
+      begin
+       IDEX_rdA <= 32'b0;    
+       IDEX_rdB <= 32'b0;
+       IDEX_signExtend <= 32'b0;
+       IDEX_instr_rd <= 5'b0;
+       IDEX_instr_rs <= 5'b0;
+       IDEX_instr_rt <= 5'b0;
+       IDEX_RegDst <= 1'b0;
+       IDEX_ALUcntrl <= 2'b0;
+       IDEX_ALUSrc <= 1'b0;
+       IDEX_Branch <= 1'b0;
+       IDEX_MemRead <= 1'b0;
+       IDEX_MemWrite <= 1'b0;
+       IDEX_MemToReg <= 1'b0;                  
+       IDEX_RegWrite <= 1'b0;
+    end 
+    else 
+      begin
+       IDEX_rdA <= rdA;
+       IDEX_rdB <= rdB;
+       IDEX_signExtend <= signExtend;
+       IDEX_instr_rd <= instr_rd;
+       IDEX_instr_rs <= instr_rs;
+       IDEX_instr_rt <= instr_rt;
+       IDEX_RegDst <= RegDst;
+       IDEX_ALUcntrl <= ALUcntrl;
+       IDEX_ALUSrc <= ALUSrc;
+       IDEX_Branch <= Branch;
+       IDEX_MemRead <= MemRead;
+       IDEX_MemWrite <= MemWrite;
+       IDEX_MemToReg <= MemToReg;                  
+       IDEX_RegWrite <= RegWrite;
+    end
+  end
+
+// Main Control Unit 
+control_main control_main (RegDst,
+                  Branch,
+                  MemRead,
+                  MemWrite,
+                  MemToReg,
+                  ALUSrc,
+                  RegWrite,
+                  ALUcntrl,
+                  opcode);
+                  
+// TO FILL IN: Instantiation of Control Unit that generates stalls
 
 
- // Calculate next pc location  
- assign pcDefiner = (BranchCond == 1) ? ~zero : zero;
- assign pc_step =  pc + 32'h4;  
- assign pc_next = (!reset) ? 32'h0 : ((Branch && pcDefiner) ? ((extended << 2)  + pc_step)  : pc_step);
+
+                           
+/***************** Execution Unit (EX)  ****************/
+                 
+assign ALUInA = IDEX_rdA;
+                 
+assign ALUInB = (IDEX_ALUSrc == 1'b0) ? IDEX_rdB : IDEX_signExtend;
+
+//  ALU
+ALU  #(32) cpu_alu(ALUOut, Zero, ALUInA, ALUInB, ALUOp);
+
+assign RegWriteAddr = (IDEX_RegDst==1'b0) ? IDEX_instr_rt : IDEX_instr_rd;
+
+ // EXMEM pipeline register
+ always @(posedge clock or negedge reset)
+  begin 
+    if (reset == 1'b0)     
+      begin
+       EXMEM_ALUOut <= 32'b0;    
+       EXMEM_RegWriteAddr <= 5'b0;
+       EXMEM_MemWriteData <= 32'b0;
+       EXMEM_Zero <= 1'b0;
+       EXMEM_Branch <= 1'b0;
+       EXMEM_MemRead <= 1'b0;
+       EXMEM_MemWrite <= 1'b0;
+       EXMEM_MemToReg <= 1'b0;                  
+       EXMEM_RegWrite <= 1'b0;
+      end 
+    else 
+      begin
+       EXMEM_ALUOut <= ALUOut;    
+       EXMEM_RegWriteAddr <= RegWriteAddr;
+       EXMEM_MemWriteData <= IDEX_rdB;
+       EXMEM_Zero <= Zero;
+       EXMEM_Branch <= IDEX_Branch;
+       EXMEM_MemRead <= IDEX_MemRead;
+       EXMEM_MemWrite <= IDEX_MemWrite;
+       EXMEM_MemToReg <= IDEX_MemToReg;                  
+       EXMEM_RegWrite <= IDEX_RegWrite;
+      end
+  end
+  
+  // ALU control
+  control_alu control_alu(ALUOp, IDEX_ALUcntrl, IDEX_signExtend[5:0]);
+  
+   // TO FILL IN: Instantiation of control logic for Forwarding goes here
+  
+
+  
+  
+  
+/***************** Memory Unit (MEM)  ****************/  
+
+// Data memory 1KB
+// Instantiate the Data Memory here 
+
+
+
+// MEMWB pipeline register
+ always @(posedge clock or negedge reset)
+  begin 
+    if (reset == 1'b0)     
+      begin
+       MEMWB_DMemOut <= 32'b0;    
+       MEMWB_ALUOut <= 32'b0;
+       MEMWB_RegWriteAddr <= 5'b0;
+       MEMWB_MemToReg <= 1'b0;                  
+       MEMWB_RegWrite <= 1'b0;
+      end 
+    else 
+      begin
+       MEMWB_DMemOut <= DMemOut;
+       MEMWB_ALUOut <= EXMEM_ALUOut;
+       MEMWB_RegWriteAddr <= EXMEM_RegWriteAddr;
+       MEMWB_MemToReg <= EXMEM_MemToReg;                  
+       MEMWB_RegWrite <= EXMEM_RegWrite;
+      end
+  end
+
+  
+  
+  
+
+/***************** WriteBack Unit (WB)  ****************/  
+// TO FILL IN: Write Back logic 
+
 
 endmodule
